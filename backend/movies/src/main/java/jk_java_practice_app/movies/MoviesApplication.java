b59e5d259d308882459616eab89cde1b96ee7d30
Jason Kwong <skjKwong@gmail.com>
2024-03-21 20:40:19 -0700

    initialized backend file struture

---
+++ backend/movies/src/main/java/jk_java_practice_app/movies/MoviesApplication.java
@@ -0,0 +1,13 @@
+package jk_java_practice_app.movies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoviesApplication.class, args);
+	}
+
+}
---
+++ backend/movies/src/test/java/jk_java_practice_app/movies/MoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package jk_java_practice_app.movies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
