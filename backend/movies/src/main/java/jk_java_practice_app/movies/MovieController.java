80f3ea5e5108a287a5b113fbc791c3f2aad038c0
Jason Kwong <skjKwong@gmail.com>
2024-04-01 21:48:29 -0700

    added new get method for searching single movie by id in controller and service layers

--- backend/movies/src/main/java/jk_java_practice_app/movies/MovieController.java
+++ backend/movies/src/main/java/jk_java_practice_app/movies/MovieController.java
@@ -1,13 +1,16 @@
 package jk_java_practice_app.movies;
 
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/v1/movies")
@@ -17,7 +20,11 @@ public class MovieController {
     private MovieService movieService;
     @GetMapping
     public ResponseEntity<List<Movie>> getAllMovies(){
-//        return new ResponseEntity<String>("All movies!", HttpStatus.OK);
         return new ResponseEntity<List<Movie>>(movieService.allMovies(), HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Optional<Movie>> getSingleMovie(@PathVariable ObjectId id){ // PathVariable: we want to convert whatever we are getting through the /{id} path to ObjectId
+        return new ResponseEntity<Optional<Movie>>(movieService.singleMovie(id), HttpStatus.OK);
     }
 }
--- backend/movies/src/main/java/jk_java_practice_app/movies/MovieService.java
+++ backend/movies/src/main/java/jk_java_practice_app/movies/MovieService.java
@@ -1,9 +1,11 @@
 package jk_java_practice_app.movies;
 
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class MovieService {
@@ -17,6 +19,12 @@ public class MovieService {
         System.out.println("All movies retrieved: " + movies.toString());
         return movies;
 
+    }
+
+    public Optional<Movie> singleMovie(ObjectId id){ // Optional is to handle return is null sitation
+        Optional<Movie> movie = movieRepository.findById(id);
+        System.out.println("Found movie: " + movie.toString());
+        return movie;
     }
 
 }
