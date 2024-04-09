821dd208bcd1fe91827e79e8324b76d25fa1d1be
Jason Kwong <skjKwong@gmail.com>
2024-04-08 22:12:53 -0700

    added controller, repository, service and postMapping for create reviews for movies

---
+++ backend/movies/src/main/java/jk_java_practice_app/movies/ReviewService.java
@@ -0,0 +1,31 @@
+package jk_java_practice_app.movies;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Update;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ReviewService {
+
+    @Autowired
+    private ReviewRepository reviewRepository;
+
+    @Autowired
+    private MongoTemplate mongoTemplate;// another way other than repository to talk to DB
+
+    public Review createReview(String reviewBody, String imdbId){
+        Review review = reviewRepository.insert(new Review(reviewBody));
+
+        mongoTemplate.update(Movie.class)
+                .matching(Criteria.where("imdbId").is(imdbId))
+                .apply(new Update().push("reviewIds").value(review))
+                .first();
+        // use a template to update movie class and update a movie where imdbid in the db matches the imdbid from the user
+        // then apply the new review value and push to the reviewIds array
+
+        return review;
+
+    }
+}
