c4a779d2b42b14d903db849fc86eb0fc46efd46f
Jason Kwong <skjKwong@gmail.com>
2024-04-09 21:31:12 -0700

    updated header in axiosConfig and added CrossOrigin annotation in backend controller to encounter cross-domain problem

--- client/react-movie-review-app/src/api/axiosConfig.js
+++ client/react-movie-review-app/src/api/axiosConfig.js
@@ -1,6 +1,12 @@
 import axios from 'axios';
 
 export default axios.create({
-  baseURL:'https://9c96-103-106-239-104.ap.ngrok.io',
-  headers: {"ngrok-skip-browser-warning": "true"}
+  // baseURL:'https://9c96-103-106-239-104.ap.ngrok.io',
+  // headers: {"ngrok-skip-browser-warning": "true"}
+
+  baseURL: 'http://localhost:8080', // Your proxy server URL
+  headers: {
+    'Content-Type': 'application/json',
+    'Access-Control-Allow-Origin': '*', // Allow requests from all origins (replace with specific origin if needed)
+  },
 })
